4c114bfc3a81f7442de5037620ab88ce4bac4683
Marcel Brilha Alves Da Silva <marcel.silva@tokiomarine.com.br>
2021-12-12 19:50:28 -0300

    Remove subscriptions

--- src/pages/Administrative.js
+++ src/pages/Administrative.js
@@ -1,4 +1,7 @@
+import { useEffect, useState, useRef } from "react";
 import { Edit, Delete, AddCircle, Search } from "@material-ui/icons";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 import {
   TableRow,
   TableCell,
@@ -11,15 +14,74 @@ import {
   Box,
   TextField,
   Pagination,
+  CircularProgress,
 } from "@material-ui/core";
 
 import Header from "../components/Header";
 import Heading from "../components/Heading";
 import Footer from "../components/Footer";
 import Style from "../styles/Administrative";
+import { search, removeSubscription } from "../services/subscription";
 
 function Administrative() {
+  const fundRef = useRef();
+  const navigate = useNavigate();
   const classes = Style();
+  const [subscriptions, setSubscriptions] = useState([]);
+  const [params, setParams] = useState({});
+  const [totalPages, setTotalPages] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function loadSubscriptions() {
+      try {
+        setLoading(true);
+        const { data } = await search(params);
+        setSubscriptions(data.content);
+        setTotalPages(data.totalPages);
+      } catch (error) {
+        swal("Error", "Ocorreu um erro ao carregar as informações", "error");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadSubscriptions();
+  }, [params]);
+
+  function paginate(event, page) {
+    const currPage = page - 1;
+    setParams({ ...params, page: currPage });
+  }
+
+  function searchFund() {
+    const value = fundRef.current.value;
+    const fundo = String(value).toLocaleUpperCase();
+    setParams({ fundo });
+  }
+
+  async function remove(id) {
+    const value = await swal("Deseja realmente remover essa subscrição?", {
+      buttons: {
+        cancel: "Cancelar",
+        remove: {
+          text: "Remover",
+          value: "remove",
+        },
+      },
+    });
+
+    try {
+      if (value === "remove") {
+        await removeSubscription(id);
+        const newSubscriptions = subscriptions.filter((curr) => curr.id !== id);
+        setSubscriptions(newSubscriptions);
+        swal("Removido", "Subscrição removida com sucesso", "success");
+      }
+    } catch (error) {
+      swal("Erro", "Ocorreu um erro ao realizar exclusão", "error");
+    }
+  }
 
   return (
     <>
@@ -30,16 +92,23 @@ function Administrative() {
 
         <nav className={classes.containerNavigation}>
           <Box className={classes.containerSearch}>
-            <TextField label="Pesquisar Fundo" variant="standard" />
+            <TextField
+              label="Pesquisar Fundo"
+              variant="standard"
+              inputRef={fundRef}
+            />
 
             <Tooltip title="Pesquisar Fundo">
-              <Search className={classes.iconSearch} />
+              <Search className={classes.iconSearch} onClick={searchFund} />
             </Tooltip>
           </Box>
 
           <Box className={classes.containerAdd}>
             <Tooltip title="Criar">
-              <AddCircle className={classes.addButton} />
+              <AddCircle
+                className={classes.addButton}
+                onClick={() => navigate("/subscription")}
+              />
             </Tooltip>
           </Box>
         </nav>
@@ -55,35 +124,66 @@ function Administrative() {
               </TableRow>
             </TableHead>
 
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Tooltip title="Editar">
-                    <IconButton size="small">
-                      <Edit />
-                    </IconButton>
-                  </Tooltip>
+            {!loading && subscriptions && subscriptions.length > 0 && (
+              <TableBody>
+                {subscriptions.map((subscription, index) => (
+                  <TableRow key={`subscription-${index}`}>
+                    <TableCell>
+                      <Tooltip title="Editar">
+                        <IconButton size="small">
+                          <Edit />
+                        </IconButton>
+                      </Tooltip>
 
-                  <Tooltip title="Remover">
-                    <IconButton size="small" className={classes.addRemove}>
-                      <Delete color="secondary" />
-                    </IconButton>
-                  </Tooltip>
-                </TableCell>
+                      <Tooltip title="Remover">
+                        <IconButton
+                          size="small"
+                          className={classes.addRemove}
+                          onClick={() => remove(subscription.id)}
+                        >
+                          <Delete color="secondary" />
+                        </IconButton>
+                      </Tooltip>
+                    </TableCell>
 
-                <TableCell>BCFF11</TableCell>
-                <TableCell>10ª</TableCell>
-                <TableCell>Andamento</TableCell>
-              </TableRow>
-            </TableBody>
+                    <TableCell>{subscription.fundo}</TableCell>
+                    <TableCell>{subscription.emissao}</TableCell>
+                    <TableCell>{subscription.etapa}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            )}
+
+            {!loading && (!subscriptions || subscriptions.length === 0) && (
+              <TableBody>
+                <TableRow>
+                  <TableCell colSpan={4} align="center">
+                    Sem registros
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            )}
+
+            {!!loading && (
+              <TableBody>
+                <TableRow>
+                  <TableCell colSpan={4} align="center">
+                    <CircularProgress color="inherit" />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            )}
           </Table>
         </TableContainer>
 
-        <Pagination
-          count={10}
-          color="secondary"
-          className={classes.pagination}
-        />
+        {!!totalPages && totalPages !== 1 && (
+          <Pagination
+            count={totalPages}
+            color="secondary"
+            className={classes.pagination}
+            onChange={paginate}
+          />
+        )}
       </section>
 
       <Footer />
--- src/services/subscription.js
+++ src/services/subscription.js
@@ -3,3 +3,11 @@ import api from "./api";
 export function create(data) {
   return api.post("/final12-subscription/api/v1/subscricoes", data);
 }
+
+export function search(params) {
+  return api.get("/final12-subscription/api/v1/subscricoes", { params });
+}
+
+export function removeSubscription(id) {
+  return api.delete(`/final12-subscription/api/v1/subscricoes/${id}`);
+}
